a939f7eed8fee14cf5eedb6808e7956377afb773
Adam Moran <noears6@gmail.com>
2024-04-28 19:19:44 -0700

    Turns out the assetdelivery works, so use that instead you mongol

--- 2013E/FixAssetLinks.lua
+++ 2013E/FixAssetLinks.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a5d3dc12ae1e015b5f42a28b50ed869dd3313c5a3372419c13d5239cca0bed2
-size 3309
+oid sha256:281dd73fe4ca67c86af8e657a675ca8c7ae91b06aae52bfc834a283a550833d3
+size 3315
