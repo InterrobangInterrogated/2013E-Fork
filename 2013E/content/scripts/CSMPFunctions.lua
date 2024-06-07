d0f334f2df7082cb8691cc7e33336d4c0a85b39f
Adam Moran <noears6@gmail.com>
2024-06-07 14:57:31 -0700

    Fixed TopLeftControl removal error in CSMPFunctions.lua

--- 2013E/content/scripts/CSMPFunctions.lua
+++ 2013E/content/scripts/CSMPFunctions.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98c3c244a36b07e295bee88f7fb8f8a885aaa27425173b0b1e2d42a076e8c40e
-size 35354
+oid sha256:c2d88f5b73f21d64557f06e1c0548dd1069abd8bd65a3b6530292f426b322e35
+size 35367
