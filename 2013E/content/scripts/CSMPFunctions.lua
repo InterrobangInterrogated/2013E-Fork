ff8e7c981a0b98f964582a42b409c48178d178fd
Adam Moran <noears6@gmail.com>
2024-06-14 12:40:54 -0700

    fixed stamper url

--- 2013E/content/scripts/CSMPFunctions.lua
+++ 2013E/content/scripts/CSMPFunctions.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5976b2e75815cb6388a853ca54a400ac9cd579dad9eab821cc8b675c6c342038
-size 36955
+oid sha256:40c8804a8bb63491fcdf8fdd32e470f16301f344b7fe9e1f1194eab9ad8f8bfe
+size 38335
